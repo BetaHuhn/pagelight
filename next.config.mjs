dd613598210764e86c8a366e2319a8474e71a9df
Maximilian Schiller <hello@mxis.ch>
2026-02-22 21:30:00 +0100

    chore: setup prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [new URL('https://avatars.githubusercontent.com/u/**?v=4')],
+    remotePatterns: [new URL("https://avatars.githubusercontent.com/u/**?v=4")],
   },
 };
 
